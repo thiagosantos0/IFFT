38498dd496460fb812a5f55b5e46fcac6e844885
thigosantos0 <thigosantosgg0@gmail.com>
2024-04-11 11:22:16 -0300

    Adding a banner for the app

---
+++ banner.py
@@ -0,0 +1,33 @@
+def banner():
+    print("\033[1;36m")    
+    print("\n")
+    print("██╗███████╗████████╗████████╗████████╗")
+    print("██║██╔════╝╚══██╔══╝╚══██╔══╝╚══██╔══╝")
+    print("██║█████╗     ██║      ██║      ██║   ")
+    print("██║██╔══╝     ██║      ██║      ██║   ")
+    print("██║██║        ██║      ██║      ██║   ")
+    print("╚═╝╚═╝        ╚═╝      ╚═╝      ╚═╝   ")                                      
+    print("\n") 
+    print("██████╗          ██╗       ██████╗    ")
+    print("██╔══██╗         ╚██╗     ██╔═══██╗   ")
+    print("██████╔╝    █████╗╚██╗    ██║   ██║   ")
+    print("██╔═══╝     ╚════╝██╔╝    ██║▄▄ ██║   ")
+    print("██║              ██╔╝     ╚██████╔╝   ")
+    print("╚═╝              ╚═╝       ╚══▀▀═╝    ")
+    print("\033[0m")    
+    print("\033[1;36m")
+    print("   IFTTT v1.0")
+    print("   Developed by: Thiago Santos")
+    print("   GitHub: github.com/thiagosantos0")
+    print("\033[0m")    
+    print("\n")
+
+def main_menu():    
+    print("1. Run")    
+    print("2. Change Pattern")    
+    print("3. Exit")    
+    print("\n")    
+    
+if __name__ == "__main__":    
+    banner()    
+    main_menu()
