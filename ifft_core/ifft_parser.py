34a84938668fbe77741e722b317003aa5a2c0574
thigosantos0 <thigosantosgg0@gmail.com>
2024-05-02 19:14:27 -0300

    #refactor Separating the single file processing logic of all repository files processing
    This refactoring is motivated by the 'single responsability principle'

--- ifft_core/ifft_parser.py
+++ ifft_core/ifft_parser.py
@@ -10,12 +10,12 @@ load_dotenv()
 log_level = os.getenv("LOG_LEVEL")
 logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def analyze_repo(project_path=''):
+def analyze_repo(project_path=dir_path_mock_project):
     '''
         Analyse the git repository and perform a scan for IFFT blocks on 
         staged and unstaged files.
     '''
-    project_path = dir_path_mock_project
+    project_path = project_path 
     print(f"Project path: {project_path}")
     logging.info("Checking staged and unstaged changes in: {}".format(project_path))
     
@@ -42,50 +42,64 @@ def analyze_repo(project_path=''):
     return results
 
 
-def scan_files(project_path=''):
+def scan_file(project_path, filename):
     '''
-        Scan through the files of a repository and find out the content inside the IFTTs blocks.
+        Scan through a file and find out the content inside the IFTTs blocks.
     '''
-    project_path = dir_path_mock_project
+    results = []
     in_block = False
     block_content = ""
     associated_file = ""
     block_start = 0
     block_end = 0
 
+    logging.debug("Scanning file: " + filename)
+    file_path = project_path + filename
+    logging.debug("File path: " + file_path)
+    lines = open(file_path).readlines()
 
-    results = []
+
+    for line_number, line in enumerate(lines):
+        if line.strip().startswith("#IFFT.If"):
+            logging.debug("Entering IFFT block" + line)
+            in_block = True
+            block_start = line_number
+            block_content += line
+
+        elif line.strip().startswith("#IFFT.Then"):
+            logging.info("Exiting IFFT block" + line)
+            associated_file = line.strip().split('(')[1].split(')')[0]
+            associated_file_name = associated_file.split(',')[0]
+            associated_file_label = associated_file.split(',')[1].strip()
+            logging.info(f"Associated file name: {associated_file_name}")
+            logging.info(f"Associated file label: {associated_file_label}")
+            block_end = line_number
+            results.append((block_content, associated_file_name, associated_file_label))
+            logging.info(f"Block content: \n{block_content}")
+            logging.info("Block end found at line: {}".format(block_end))
+            in_block = False
+            block_content = ""
+
+        elif in_block:
+            block_content += line
+
+    return results
+
+def scan_files(project_path=dir_path_mock_project):
+    '''
+        Scan through the files of a repository and find out the content inside the IFTTs blocks.
+    '''
+    project_path = project_path
+
+
+    results_dict = {}
 
     # scanning_files
     for filename in os.listdir(project_path):
         if filename.endswith(".py"):
-            logging.debug("Scanning file: " + filename)
-            file_path = project_path + filename
-            lines = open(file_path).readlines()
-        
+            results_dict[filename] = scan_file(project_path, filename)
 
-            for line_number, line in enumerate(lines):
-                if line.strip().startswith("#IFFT.If"):
-                    logging.debug("Entering IFFT block" + line)
-                    in_block = True
-                    block_start = line_number
-                    block_content += line
 
-                elif line.strip().startswith("#IFFT.Then"):
-                    logging.info("Exiting IFFT block" + line)
-                    associated_file = line.strip().split('(')[1].split(')')[0]
-                    associated_file_name = associated_file.split(',')[0]
-                    associated_file_label = associated_file.split(',')[1]
-                    logging.info(f"Associated file name: {associated_file_name}")
-                    logging.info(f"Associated file label: {associated_file_label}")
-                    block_end = line_number
-                    results.append((block_content, associated_file_name))
-                    logging.info(f"Block content: \n{block_content}")
-                    logging.info("Block end found at line: {}".format(block_end))
-                    in_block = False
-
-                elif in_block:
-                    block_content += line
-    
+    print(f"Results dict: {results_dict}")
     # Returning the results of the first IFFT block found in the file
-    return results
+    return results_dict 
