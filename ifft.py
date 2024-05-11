62d73d6669c7b67fb7fd9906738be4211fd3686d
thigosantos0 <thigosantosgg0@gmail.com>
2024-05-11 16:29:53 -0300

    Changes in the main program for the demo

--- ifft.py
+++ ifft.py
@@ -8,6 +8,12 @@ from ifft_core.ifft_parser import *
 def main():
     results = analyze_repo()
 
+    # Changes in IFFT blocks 
+    print(f"Changed identified inside a IFFT block:\n\n {results['app.py'][0][0]}")
+    print(f"Should also modify: {results['app.py'][0][1]}\nBlock label: {results['app.py'][0][2]}")
+
+
+
 if __name__ == "__main__":
     main()
 
