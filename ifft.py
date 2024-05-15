079c97f94aabbcb41320f45beaabde61bcb4af2e
Thiago Santos <thiagosantosgg0@gmail.com>
2024-05-15 14:20:50 -0300

    Fix bug #2
    This change handles the bug where the tool was being triggered even when we didn't have any change or when the change was outside a IFFT block. Now the tool is triggered only when a change is made in the repository and this change is placed inside a IFFT block.

--- ifft.py
+++ ifft.py
@@ -1,3 +1,4 @@
+
 # Main file for IFTT linter
 
 import sys
@@ -6,13 +7,13 @@ import os
 from ifft_core.ifft_parser import *
 
 def main():
-    results = analyze_repo()
+    results = scan_files()
 
-    # Changes in IFFT blocks 
-    print(f"Changed identified inside a IFFT block:\n\n {results['app.py'][0][0]}")
-    print(f"Should also modify: {results['app.py'][0][1]}\nBlock label: {results['app.py'][0][2]}")
-
-
+    for file, blocks in results.items():
+        for block in blocks:
+            print(f"Changed identified inside a IFFT block in {file}:\n\n {block['block_content']}")
+            print(f"Modified lines within the block: {block['modified_lines_within_block']}")
+            print(f"Should also modify: {block['associated_file_name']}\nBlock label: {block['associated_file_label']}\n")
 
 if __name__ == "__main__":
     main()
--- ifft_core/ifft_parser.py
+++ ifft_core/ifft_parser.py
@@ -1,11 +1,11 @@
-from git import Repo
-from dotenv import load_dotenv
+from git import Repo, InvalidGitRepositoryError, NoSuchPathError
 import os
 import logging
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
+from dotenv import load_dotenv
 
 file_dir = os.path.dirname(__file__)
-dir_path_mock_project = os.path.join(file_dir, '..', './mock_project/')
+dir_path_mock_project = os.path.join(file_dir, '..', 'mock_project')
 
 load_dotenv()
 log_level = os.getenv("LOG_LEVEL")
@@ -13,10 +13,6 @@ logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(mes
 
 
 def validate_associated_file(associated_file_name):
-    '''
-        Validate the associated file name and label.
-    '''
-    # starting from the root of the project
     project_path = os.path.join(dir_path_mock_project)
     associated_file_name = associated_file_name.replace('"', '')
     file_path = os.path.join(project_path, associated_file_name)
@@ -25,58 +21,32 @@ def validate_associated_file(associated_file_name):
         logging.error(f"{Fore.RED} Associated file path: {file_path} {Style.RESET_ALL}")
         return False
     logging.info(f"{Fore.YELLOW} Associated file: {associated_file_name} found {Style.RESET_ALL}")
-
     return True
 
-def analyze_repo(project_path=dir_path_mock_project):
-    '''
-        Analyse the git repository and perform a scan for IFFT blocks on 
-        staged and unstaged files.
-    '''
-    project_path = project_path 
-    print(f"{Fore.BLUE} Project path: {project_path} {Style.RESET_ALL}")
-    logging.info(f"{Fore.YELLOW} Checking staged and unstaged changes in: {project_path} {Style.RESET_ALL}")
-    
+def get_modified_lines(repo, filename):
+    """Get a set of modified lines for the given filename."""
+    modified_lines = set()
+    diff_text = repo.git.diff(None, filename)
+    for line in diff_text.split('\n'):
+        if line.startswith('+') and not line.startswith('+++'):
+            modified_lines.add(line[1:].strip())
+    print(f"{Fore.GREEN} Modified lines: {modified_lines} {Style.RESET_ALL}")
+    return modified_lines
 
-    try:
-        repo = Repo(project_path)
-
-    except Exception as error:
-        logging.error(f"{Fore.RED} Failed to load repository: {error} {Style.RESET_ALL}")
-        return []
-
-    staged_files = [item.a_path for item in repo.index.diff("HEAD")]
-    #logging.info("Staged files: {}".format(' '.join(map(str, staged_files))))
-    logging.info(Fore.YELLOW+ "Staged files: {}".format(' '.join(map(str, staged_files))) + Style.RESET_ALL)
-    if not staged_files:
-        logging.debug(f"{Fore.GREEN} No staged files found {Style.RESET_ALL}")
-
-    unstaged_files = [item.a_path for item in repo.index.diff(None)]
-    logging.info(Fore.YELLOW + "Unstaged files: {}".format(' '.join(map(str, unstaged_files))) + Style.RESET_ALL)
-    if not unstaged_files:
-        logging.debug(f"{Fore.GREEN} No unstaged files found {Style.RESET_ALL}")
-
-    results = scan_files(project_path)
-
-    return results
-
-
-def scan_file(project_path, filename):
-    '''
-        Scan through a file and find out the content inside the IFTTs blocks.
-    '''
+def scan_file(project_path, filename, modified_lines_set):
     results = []
     in_block = False
     block_content = ""
     associated_file = ""
     block_start = 0
     block_end = 0
+    modified_lines_within_blocks = []
 
     logging.debug(f"{Fore.GREEN} Scanning file: {filename} {Style.RESET_ALL}")
-    file_path = project_path + filename
+    file_path = os.path.join(project_path, filename)
     logging.debug(f"{Fore.GREEN} File path: {file_path} {Style.RESET_ALL}")
-    lines = open(file_path).readlines()
 
+    lines = open(file_path).readlines()
 
     for line_number, line in enumerate(lines):
         if line.strip().startswith("#IFFT.If"):
@@ -84,15 +54,11 @@ def scan_file(project_path, filename):
             in_block = True
             block_start = line_number
             block_content += line
-
         elif line.strip().startswith("#IFFT.Then"):
             logging.info(f"{Fore.YELLOW} Exiting IFFT block {line} + {Style.RESET_ALL}")
             associated_file = line.strip().split('(')[1].split(')')[0]
-            print(f"{Fore.BLUE} Associated file: {associated_file} {Style.RESET_ALL}")
             associated_file_name = associated_file.split(',')[0]
-            print(f"{Fore.BLUE} Associated filename: {associated_file_name} {Style.RESET_ALL}")
             associated_file_label = associated_file.split(',')[1].strip()
-            # Check if the associated file exists
             valid_associated_file = validate_associated_file(associated_file_name)
             if not valid_associated_file:
                 associated_file_name = ""
@@ -100,32 +66,47 @@ def scan_file(project_path, filename):
             logging.info(f"{Fore.YELLOW} Associated file name: {associated_file_name} {Style.RESET_ALL}")
             logging.info(f"{Fore.YELLOW} Associated file label: {associated_file_label} {Style.RESET_ALL}")
             block_end = line_number
-            results.append((block_content, associated_file_name, associated_file_label))
+
+            results.append({
+                "block_content": block_content,
+                "associated_file_name": associated_file_name,
+                "associated_file_label": associated_file_label,
+                "modified_lines_within_block": modified_lines_within_blocks
+            })
+
             logging.info(f"{Fore.YELLOW} Block content: \n{block_content} {Style.RESET_ALL}")
             logging.info(f"{Fore.YELLOW} Block end found at line: {block_end} {Style.RESET_ALL}")
             in_block = False
             block_content = ""
-
+            modified_lines_within_blocks = []
         elif in_block:
             block_content += line
+            if line.strip() in modified_lines_set:
+                modified_lines_within_blocks.append(line.strip())
 
     return results
 
 def scan_files(project_path=dir_path_mock_project):
-    '''
-        Scan through the files of a repository and find out the content inside the IFTTs blocks.
-    '''
-    project_path = project_path
-
-
     results_dict = {}
 
-    # scanning_files
-    for filename in os.listdir(project_path):
+    try:
+        repo = Repo(project_path)
+    except NoSuchPathError:
+        logging.error(f"{Fore.RED} The path '{project_path}' does not exist. {Style.RESET_ALL}")
+        return results_dict
+    except InvalidGitRepositoryError:
+        logging.error(f"{Fore.RED} The path '{project_path}' is not a valid Git repository. {Style.RESET_ALL}")
+        return results_dict
+    except Exception as e:
+        logging.error(f"{Fore.RED} Failed to load repository: {e} {Style.RESET_ALL}")
+        return results_dict
+
+    unstaged_files = [item.a_path for item in repo.index.diff(None)]
+
+    for filename in unstaged_files:
         if filename.endswith(".py"):
-            results_dict[filename] = scan_file(project_path, filename)
+            modified_lines_set = get_modified_lines(repo, filename)
+            results_dict[filename] = scan_file(project_path, filename, modified_lines_set)
 
+    return results_dict
 
-    print(f"Results dict: {results_dict}")
-    # Returning the results of the first IFFT block found in the file
-    return results_dict 
