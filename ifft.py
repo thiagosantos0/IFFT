381ee7fcb15302a1946ea167040ee8bfd5c40e65
thigosantos0 <thigosantosgg0@gmail.com>
2024-05-24 07:40:14 -0300

    Editing main file to not print recommendation message when it's not the case
    This commit is a continuation for the commit that fixed the bug #7

--- ifft.py
+++ ifft.py
@@ -16,7 +16,8 @@ def main():
         for block in blocks:
             print(f"Changed identified inside a IFFT block in {file}:\n\n {block['block_content']}")
             print(f"Modified lines within the block: {block['modified_lines_within_block']}")
-            print(f"Should also modify: {block['associated_file_name']}\nBlock label: {block['associated_file_label']}\n")
+            if block['modified_lines_within_block'] != []:
+                print(f"Should also modify: {block['associated_file_name']}\nBlock label: {block['associated_file_label']}\n")
 
 if __name__ == "__main__":
     main()
