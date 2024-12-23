f1df1c1096b432bb22a3c05fd53f91600af135f1
Thiago Santos <thiagosantosgg0@gmail.com>
2024-12-23 10:12:06 -0300

    Adding new option that counts the amount of ifft blocks inside the project
    I also did some structural changes in the code: Those new options will not be activated in 'auto_mode' version as it does not makes sense to have an option like this in auto_mode version. More details on this will be mentioned in the tool documentation.

--- ifft.py
+++ ifft.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import logging
 import os
+from block_manager.block_manager_class import BlockManager
 from ifft_core.ifft_parser import scan_files
 
 def load_config():
@@ -22,6 +23,7 @@ def main(auto_mode=False):
     # Tool opt-in configs
     debug_mode = config.get('debug_mode', False)
     auto_mode = config.get('auto_mode', False)
+    show_active_blocks = config.get('show_active_blocks', False)
 
     if debug_mode:
         logging.getLogger().setLevel(logging.DEBUG)
@@ -32,20 +34,35 @@ def main(auto_mode=False):
     config_path = os.path.join(os.path.dirname(__file__), 'ifft_config.json')
     logging.debug(f"Config path: {config_path}")
     
-    if os.path.exists(config_path):
-        with open(config_path) as config_file:
-            config = json.load(config_file)
-            auto_mode = config.get('auto_mode', auto_mode)
-            logging.debug(f"Auto mode set to: {auto_mode}")
+    # if os.path.exists(config_path):
+    #     with open(config_path) as config_file:
+    #         config = json.load(config_file)
+    #         auto_mode = config.get('auto_mode', auto_mode)
+    #         logging.debug(f"Auto mode set to: {auto_mode}")
     
 
     logging.debug("Starting IFFT scan.")
     results = scan_files(auto_mode=auto_mode)
+
+
     
     if not results:
         logging.debug("No results found from scan_files.")
     else:
         logging.debug(f"Results found: {results}")
+
+
+    # All options besides debug_mode will be disabled in auto_mode version
+    if not auto_mode:
+        # Create a block manager object to manage the blocks
+        block_manager = BlockManager()
+
+
+        # ---------------------------------------------------
+        # Feature: Get total active blocks in the project
+        if show_active_blocks:
+            block_manager.get_block_count()
+
     
     changes_detected = False
     for file, blocks in results.items():
