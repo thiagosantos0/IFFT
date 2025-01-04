74a981e2544ab717ebfc9f5edb2accfdf945449d
thigosantos0 <thigosantosgg0@gmail.com>
2025-01-04 16:06:00 -0300

    #ifft-web-interface Changing the address for the output viewer
    I think that output-viewer is a better name than just output for the page as we'll have more features that deals with output

--- IFFT_WEB/modules/output.py
+++ IFFT_WEB/modules/output.py
@@ -4,7 +4,7 @@ import json
 
 output_bp = Blueprint('output', __name__)
 
-@output_bp.route('/output')
+@output_bp.route('/output-viewer')
 def show_output():
     results = json.load(open('data/ifft_results.json'))
     return render_template('output.html', results_dict=results)
